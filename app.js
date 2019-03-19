7cf9008e4ca8d9b27db597b64a6e1c4f6160d3b4
wangzm <wangzm@channelsoft.com>
2019-03-19 17:48:23 +0800

    使用express-session记住登录状态

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const path = require('path');
 const router = require('./router');
+const session = require('express-session');
+
 let app = express();
 const bodyParser = require('body-parser');
 // parser application/x-www-form-urlencode
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
 // parser application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+// 使用express-session中间件
+app.use(session({
+    // 配置加密字符串，会在原
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}))
+
 app.use(router);
 
 app.use('/public/', express.static(path.join(__dirname, './public/')))
--- router.js
+++ router.js
@@ -7,19 +7,37 @@ const crypto = require('crypto');
 const hash = crypto.createHash('md5');
 
 router.get('/', (req,res)=>{
-    res.render('index.html');
+    console.log("/ " + JSON.stringify(req.session.user));
+    res.render('index.html',{
+        user: req.session.user
+    });
 })
 // 登录
 router.get('/login', (req,res)=>{
     res.render('login.html');
 })
+
 router.post('/login', (req,res)=>{
-    // res.render('index.html');
+    console.log(req.body);
+    let body = req.body;
+    hash.update(body.password);
+    body.password = hash.digest('hex');
+    console.log('login: password' + body.password);
+    User.findOne({email: body.email, password: body.password}, function(err, res){
+        if(err){
+            return res.status(500).json({code: 500, msg: 'server error'})
+        }
+        if(res){
+            return res.status(200).json({code: 0, mag: 'login success'})
+        }
+    })
 })
+
 // 注册
 router.get('/register', (req,res)=>{
     res.render('register.html');
 })
+
 router.post('/register', (req,res)=>{
     let body = req.body;
     console.log(body);
@@ -38,13 +56,16 @@ router.post('/register', (req,res)=>{
         }
 
         let user = new User(body);
-        hash.update(user.password);
-        user.password = hash.digest('hex');
-        console.log('user.password: ' + user.password);
+        // hash.update(user.password);
+        // user.password = hash.digest('hex');
+        // console.log('register.password: ' + user.password);
         user.save(function(err, user){
             if(err){
                 return res.status(500).send(JSON.stringify({code: 500, msg: 'server error'}))
             }
+            // 使用session存储当前用户
+            req.session.user = user;
+            
             // res.status(200).send(JSON.stringify({code: 0, msg: '注册成功'}));
             // 使用express提供的响应方法json代替，会自动将对象转为字符串
             res.status(200).json({code: 0, msg: '注册成功'})
